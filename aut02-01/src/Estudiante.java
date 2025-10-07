9db8c4e8e1f8963720aaf41ada1314e82f60e526
UnaiUzquizaLeal <unaizaquizu.7@gmail.com>
2025-10-07 19:56:32 +0100

    Añadir un nuevo estudiante solicitando al usuario sus datos personales y académicos.

---
+++ aut02-01/src/Estudiante.java
@@ -0,0 +1,32 @@
+public class Estudiante {
+    private String nombre;
+    private String apellido1;
+    private String apellido2;
+    private int edad;
+    private double nota;
+
+    // Constructor
+    public Estudiante(String nombre, String apellido1, String apellido2, int edad, double nota) {
+        this.nombre = nombre;
+        this.apellido1 = apellido1;
+        this.apellido2 = apellido2;
+        this.edad = edad;
+        this.nota = nota;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+    public String getApellido1() {
+        return this.apellido1;
+    }
+    public String getApellido2() {
+        return this.apellido2;
+    }
+    public int getEdad() {
+        return this.edad;
+    }
+    public double getNota() {
+        return this.nota;
+    }
+}
--- aut02-01/src/Main.java
+++ aut02-01/src/Main.java
@@ -1,5 +1,57 @@
-public class Main {
-    public static void main() {
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int opcion = 0;
+        boolean condicion = true;
+
+        do {
+            System.out.println("\n>>> MENU ");
+            System.out.println("  1. Crear estudiante");
+            System.out.println("  3. Salir");
+            System.out.print(" >> Elige una opcion > ");
+            opcion = Integer.parseInt(sc.nextLine());
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("  > Introduce los datos del estudiante ");
+                    System.out.print("   > Introduce el nombre: ");
+                    String nombre = sc.nextLine();
+                    System.out.print("   > Introduce el primer apellido: ");
+                    String apellido1 = sc.nextLine();
+                    System.out.print("   > Introduce el segundo apellido: ");
+                    String apellido2 = sc.nextLine();
+                    System.out.print("   > Introduce la edad: ");
+                    int edad = sc.nextInt();
+                    sc.nextLine();
+                    System.out.print("   > Introduce la nota media del estudiante: ");
+                    String notaStr = sc.nextLine().replace(",", ".");
+                    double nota = Double.parseDouble(notaStr);
+
+                    Estudiante p1 = new Estudiante(nombre, apellido1, apellido2, edad, nota);
+
+                    System.out.println("\n >> Datos del o la estudiante <<");
+                    System.out.printf(
+                            "Datos del Estudiante > %s %s %s, tiene %d años, su nota media es de %.2f. %n", p1.getNombre(), p1.getApellido1(), p1.getApellido2(), p1.getEdad(), p1.getNota()
+                    );
+                    break;
+
+                case 2:
+                    System.out.println("Opción 2 seleccionada");
+                    break;
+
+                case 3:
+                    System.out.println("Saliendo del programa...");
+                    condicion = false;
+                    break;
+
+                default:
+                    System.out.println("Opción no válida. Intenta de nuevo.");
+                    break;
+            }
+        } while (condicion);
+
+        sc.close();
     }
 }
