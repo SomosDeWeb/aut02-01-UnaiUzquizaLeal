6d2c95133758853199c9b9324e26c3419ab52388
UnaiUzquizaLeal <unaizaquizu.7@gmail.com>
2025-10-07 21:56:08 +0100

    Permitir salir del programa y rer eestructuraccion del codigo

--- aut02-01/src/Main.java
+++ aut02-01/src/Main.java
@@ -1,6 +1,6 @@
-import java.util.Collections;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Collections;
 
 
 public class Main {
@@ -12,124 +12,30 @@ public class Main {
 
 
         do {
-            System.out.println("\n>>> MENU ");
-            System.out.println("  1. Crear estudiante");
-            System.out.println("  2. Listar todos los estudiantes");
-            System.out.println("  3. Buscar estudiante por su nombre");
-            System.out.println("  4. Calcular la media de todas las notas");
-            System.out.println("  5. Mostrar al estudiante con la mejor calificación");
-            System.out.println("  6. Salir");
+            menu();
             System.out.print("\n >> Elige una opcion > ");
             opcion = Integer.parseInt(sc.nextLine());
 
-
             switch (opcion) {
                 case 1:
-                    System.out.println("  > Introduce los datos del estudiante ");
-                    System.out.print("   > Introduce el nombre: ");
-                    String nombre = sc.nextLine();
-                    System.out.print("   > Introduce el primer apellido: ");
-                    String apellido1 = sc.nextLine();
-                    System.out.print("   > Introduce el segundo apellido: ");
-                    String apellido2 = sc.nextLine();
-                    System.out.print("   > Introduce la edad: ");
-                    int edad = sc.nextInt();
-                    sc.nextLine();
-                    System.out.print("   > Introduce la nota media del estudiante: ");
-                    String notaStr = sc.nextLine().replace(",", ".");
-                    double nota = Double.parseDouble(notaStr);
-
-                    Estudiante p1 = new Estudiante(nombre, apellido1, apellido2, edad, nota);
-                    listaEstudiantes.add(p1);
-
-
-                    System.out.println("\n >> Datos del o la estudiante <<");
-                    System.out.printf(
-                            "Datos del Estudiante > %s %s %s, tiene %d años, su nota media es de %.2f. %n", p1.getNombre(), p1.getApellido1(), p1.getApellido2(), p1.getEdad(), p1.getNota()
-                    );
+                    crearEstudiante(listaEstudiantes, sc);
                     break;
-
                 case 2:
-                    int contador = 0;
-                    if (listaEstudiantes.isEmpty()) {
-                        System.out.println(" \n2>> No hay estudiantes registrados");
-                    }
-                    else {
-                        for (Estudiante i : listaEstudiantes) {
-                            ++contador;
-                            System.out.printf(
-                            "> Estudiante n%d: %s %s %s, %d años, nota media: %.2f.%n",
-                                    contador, i.getNombre(), i.getApellido1(), i.getApellido2(), i.getEdad(), i.getNota()
-                            );
-                        }
-                    }
-                    contador = 0;
+                    listarEstudiantes(listaEstudiantes);
                     break;
                 case 3:
-
-                    System.out.print("  >> Introduce el nombre: ");
-                    String buscarNombre = sc.nextLine();
-                    boolean encontrado = false;
-
-                    for (Estudiante i : listaEstudiantes) {
-                        if (i.getNombre().equalsIgnoreCase(buscarNombre)) {
-                            System.out.printf(
-                                    "   > Estudiante encontrado: %s %s %s, %d años, nota media: %.2f.%n",
-                                    i.getNombre(), i.getApellido1(), i.getApellido2(), i.getEdad(), i.getNota()
-                            );
-                            encontrado = true;
-                        }
-                    }
-
-                    if (!encontrado) {
-                        System.out.println("\n>>> No se encontró ningún estudiante con ese nombre.");
-                    }
+                    buscarEstudiante(listaEstudiantes, sc);
                     break;
-
                 case 4:
-                    double totalNotas = 0;
-                    int nNotas = 0;
-                    double mediaNotas;
-                    if (listaEstudiantes.isEmpty()) {
-                        System.out.println("\n>>> No se encontraron datos para saber el estudiante con mayor calificación");
-                    } else {
-                        for (Estudiante i : listaEstudiantes) {
-                            nNotas ++;
-                            double x = i.getNota();
-                            totalNotas += x;
-                        }
-                        mediaNotas = totalNotas / nNotas;
-                        System.out.print("   > La nota media de todos los estudiantes es " + mediaNotas);
-
-                    }
+                    calcularMedia(listaEstudiantes);
                     break;
-
                 case 5:
-                    if (listaEstudiantes.isEmpty()) {
-                        System.out.println("\n>>> No se encontraron datos para saber el estudiante con mayor calificación");
-                    } else {
-                        ArrayList<Double> listaNotas = new ArrayList<>();
-                        for (Estudiante i : listaEstudiantes) {
-                            listaNotas.add(i.getNota());
-                        }
-
-                        double notaMaxima = Collections.max(listaNotas);
-                        for (Estudiante i : listaEstudiantes) {
-                            if (i.getNota() == notaMaxima) {
-                                System.out.printf(
-                                    "   > Estudiante con la mayor media es: %s %s %s, %d años, nota media: %.2f.%n",
-                                    i.getNombre(), i.getApellido1(), i.getApellido2(), i.getEdad(), i.getNota()
-                                );
-                            }
-                        }
-                    }1
+                    mayorNota(listaEstudiantes, sc);
                     break;
-
                 case 6:
                     System.out.println("\n>>> Saliendo del programa...");
                     condicion = false;
                     break;
-
                 default:
                     System.out.println("Opción no válida. Intenta de nuevo.");
                     break;
@@ -138,4 +44,112 @@ public class Main {
 
         sc.close();
     }
+    public static void menu() {
+        System.out.println("\n>>> MENU ");
+        System.out.println("  1. Crear estudiante");
+        System.out.println("  2. Listar todos los estudiantes");
+        System.out.println("  3. Buscar estudiante por su nombre");
+        System.out.println("  4. Calcular la media de todas las notas");
+        System.out.println("  5. Mostrar al estudiante con la mejor calificación");
+        System.out.println("  6. Salir");
+
+    }
+
+    public static void crearEstudiante(ArrayList<Estudiante> listaEstudiantes, Scanner sc) {
+        System.out.println("  > Introduce los datos del estudiante ");
+        System.out.print("   > Introduce el nombre: ");
+        String nombre = sc.nextLine();
+        System.out.print("   > Introduce el primer apellido: ");
+        String apellido1 = sc.nextLine();
+        System.out.print("   > Introduce el segundo apellido: ");
+        String apellido2 = sc.nextLine();
+        System.out.print("   > Introduce la edad: ");
+        int edad = sc.nextInt();
+        sc.nextLine();
+        System.out.print("   > Introduce la nota media del estudiante: ");
+        String notaStr = sc.nextLine().replace(",", ".");
+        double nota = Double.parseDouble(notaStr);
+
+        Estudiante p1 = new Estudiante(nombre, apellido1, apellido2, edad, nota);
+        listaEstudiantes.add(p1);
+
+        System.out.printf(
+                "\n >> Datos del o la estudiante <<\nDatos del Estudiante > %s %s %s, tiene %d años, su nota media es de %.2f. %n",
+                p1.getNombre(), p1.getApellido1(), p1.getApellido2(), p1.getEdad(), p1.getNota()
+        );
+    }
+
+    public static void listarEstudiantes(ArrayList<Estudiante> listaEstudiantes) {
+        if (listaEstudiantes.isEmpty()) {
+            System.out.println(" \n>> No hay estudiantes registrados");
+        } else {
+            int contador = 0;
+            for (Estudiante i : listaEstudiantes) {
+                ++contador;
+                System.out.printf(
+                        "  > Estudiante n%d: %s %s %s, %d años, nota media: %.2f.%n",
+                        contador, i.getNombre(), i.getApellido1(), i.getApellido2(), i.getEdad(), i.getNota()
+                );
+            }
+        }
+    }
+
+    public static void buscarEstudiante(ArrayList<Estudiante> listaEstudiantes, Scanner sc) {
+        System.out.print("  >> Introduce el nombre: ");
+        String buscarNombre = sc.nextLine();
+        boolean encontrado = false;
+
+        for (Estudiante i : listaEstudiantes) {
+            if (i.getNombre().equalsIgnoreCase(buscarNombre)) {
+                System.out.printf(
+                        "   > Estudiante encontrado: %s %s %s, %d años, nota media: %.2f.%n",
+                        i.getNombre(), i.getApellido1(), i.getApellido2(), i.getEdad(), i.getNota()
+                );
+                encontrado = true;
+            }
+        }
+
+        if (!encontrado) {
+            System.out.println("\n>>> No se encontró ningún estudiante con ese nombre.");
+        }
+    }
+
+    public static void calcularMedia(ArrayList<Estudiante> listaEstudiantes) {
+        if (listaEstudiantes.isEmpty()) {
+            System.out.println("\n>>> No se encontraron datos para calcular la media");
+            return;
+        }
+
+        double totalNotas = 0;
+        for (Estudiante i : listaEstudiantes) {
+            totalNotas += i.getNota();
+        }
+
+        double mediaNotas = totalNotas / listaEstudiantes.size();
+        System.out.printf("   > La nota media de todos los estudiantes es %.2f%n", mediaNotas);
+    }
+
+    public static void mayorNota(ArrayList<Estudiante> listaEstudiantes, Scanner sc) {
+        if (listaEstudiantes.isEmpty()) {
+            System.out.println("\n>>> No se encontraron datos para saber el estudiante con mayor calificación");
+        } else {
+            ArrayList<Double> listaNotas = new ArrayList<>();
+            for (Estudiante i : listaEstudiantes) {
+                listaNotas.add(i.getNota());
+            }
+
+            double notaMaxima = Collections.max(listaNotas);
+            for (Estudiante i : listaEstudiantes) {
+                if (i.getNota() == notaMaxima) {
+                    System.out.printf(
+                            "   > Estudiante con la mayor media es: %s %s %s, %d años, nota media: %.2f.%n",
+                            i.getNombre(), i.getApellido1(), i.getApellido2(), i.getEdad(), i.getNota()
+                    );
+                }
+            }
+        }
+    }
+
 }
+
+
